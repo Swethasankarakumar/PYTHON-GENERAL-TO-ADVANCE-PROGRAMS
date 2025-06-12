bd9103d634662895a0737b8e23142f889a22c76e
Swetha Sankara Kumar <166848438+Swethasankarakumar@users.noreply.github.com>
2025-06-12 11:12:25 +0530

    Add files via upload
    This program says how to get user input for general shopping cart in python

---
+++ shoppingcart.py
@@ -0,0 +1,5 @@
+item = input("Enter the item:")
+price = float(input("Enter the price of the item:"))
+quantity = int(input("Enter the no. of items to buy:"))
+total = price*quantity
+print(f"The total amount is ${total}")
